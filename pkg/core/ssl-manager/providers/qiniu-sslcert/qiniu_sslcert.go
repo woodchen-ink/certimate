2f551dd3e3db9b5ff417f6b2ee1ff64f7e36af4a
Fu Diwei <fudiwei@sina.com>
2025-07-08 10:25:44 +0800

    fix: #854

--- pkg/core/ssl-manager/providers/qiniu-sslcert/qiniu_sslcert.go
+++ pkg/core/ssl-manager/providers/qiniu-sslcert/qiniu_sslcert.go
@@ -2,9 +2,12 @@ package qiniusslcert
 
 import (
 	"context"
+	"crypto/x509"
 	"errors"
 	"fmt"
 	"log/slog"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/qiniu/go-sdk/v7/auth"
@@ -24,7 +27,7 @@ type SSLManagerProviderConfig struct {
 type SSLManagerProvider struct {
 	config    *SSLManagerProviderConfig
 	logger    *slog.Logger
-	sdkClient *qiniusdk.CdnManager
+	sdkClient *qiniusdk.SslCertManager
 }
 
 var _ core.SSLManager = (*SSLManagerProvider)(nil)
@@ -64,12 +67,80 @@ func (m *SSLManagerProvider) Upload(ctx context.Context, certPEM string, privkey
 	// 生成新证书名（需符合七牛云命名规则）
 	certName := fmt.Sprintf("certimate-%d", time.Now().UnixMilli())
 
+	// 遍历查询已有证书，避免重复上传
+	getSslCertListMarker := ""
+	getSslCertListLimit := int32(200)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+		}
+
+		getSslCertListResp, err := m.sdkClient.GetSslCertList(context.TODO(), getSslCertListMarker, getSslCertListLimit)
+		m.logger.Debug("sdk request 'sslcert.GetList'", slog.Any("request.marker", getSslCertListMarker), slog.Any("response", getSslCertListResp))
+		if err != nil {
+			return nil, fmt.Errorf("failed to execute sdk request 'sslcert.GetList': %w", err)
+		}
+
+		if getSslCertListResp.Certs != nil {
+			for _, sslCert := range getSslCertListResp.Certs {
+				// 先对比证书通用名称
+				if !strings.EqualFold(certX509.Subject.CommonName, sslCert.CommonName) {
+					continue
+				}
+
+				// 再对比证书多域名
+				if !slices.Equal(certX509.DNSNames, sslCert.DnsNames) {
+					continue
+				}
+
+				// 再对比证书有效期
+				if certX509.NotBefore.Unix() != sslCert.NotBefore || certX509.NotAfter.Unix() != sslCert.NotAfter {
+					continue
+				}
+
+				// 最后对比证书公钥算法
+				switch certX509.PublicKeyAlgorithm {
+				case x509.RSA:
+					if !strings.EqualFold(sslCert.Encrypt, "RSA") {
+						continue
+					}
+				case x509.ECDSA:
+					if !strings.EqualFold(sslCert.Encrypt, "ECDSA") {
+						continue
+					}
+				case x509.Ed25519:
+					if !strings.EqualFold(sslCert.Encrypt, "ED25519") {
+						continue
+					}
+				default:
+					// 未知算法，跳过
+					continue
+				}
+
+				// 如果以上信息都一致，则视为已存在相同证书，直接返回
+				m.logger.Info("ssl certificate already exists")
+				return &core.SSLManageUploadResult{
+					CertId:   sslCert.CertID,
+					CertName: sslCert.Name,
+				}, nil
+			}
+		}
+
+		if len(getSslCertListResp.Certs) < int(getSslCertListLimit) || getSslCertListResp.Marker == "" {
+			break
+		} else {
+			getSslCertListMarker = getSslCertListResp.Marker
+		}
+	}
+
 	// 上传新证书
 	// REF: https://developer.qiniu.com/fusion/8593/interface-related-certificate
 	uploadSslCertResp, err := m.sdkClient.UploadSslCert(context.TODO(), certName, certX509.Subject.CommonName, certPEM, privkeyPEM)
-	m.logger.Debug("sdk request 'cdn.UploadSslCert'", slog.Any("response", uploadSslCertResp))
+	m.logger.Debug("sdk request 'sslcert.Upload'", slog.Any("response", uploadSslCertResp))
 	if err != nil {
-		return nil, fmt.Errorf("failed to execute sdk request 'cdn.UploadSslCert': %w", err)
+		return nil, fmt.Errorf("failed to execute sdk request 'sslcert.Upload': %w", err)
 	}
 
 	return &core.SSLManageUploadResult{
@@ -78,7 +149,7 @@ func (m *SSLManagerProvider) Upload(ctx context.Context, certPEM string, privkey
 	}, nil
 }
 
-func createSDKClient(accessKey, secretKey string) (*qiniusdk.CdnManager, error) {
+func createSDKClient(accessKey, secretKey string) (*qiniusdk.SslCertManager, error) {
 	if secretKey == "" {
 		return nil, errors.New("invalid qiniu access key")
 	}
@@ -88,6 +159,6 @@ func createSDKClient(accessKey, secretKey string) (*qiniusdk.CdnManager, error) 
 	}
 
 	credential := auth.New(accessKey, secretKey)
-	client := qiniusdk.NewCdnManager(credential)
+	client := qiniusdk.NewSslCertManager(credential)
 	return client, nil
 }

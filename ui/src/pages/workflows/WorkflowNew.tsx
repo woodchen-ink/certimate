4a2d9c2b99c40ddd2f6a7f29fc5095dccb8daff5
Fu Diwei <fudiwei@sina.com>
2025-07-19 22:00:28 +0800

    feat(ui): new WorkflowNew page

--- ui/src/pages/workflows/WorkflowNew.tsx
+++ ui/src/pages/workflows/WorkflowNew.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import { PageHeader } from "@ant-design/pro-components";
-import { Card, Col, Form, Input, type InputRef, Row, Spin, Typography, notification } from "antd";
+import { App, Card, Col, Form, Input, type InputRef, Row, Spin, Typography } from "antd";
 import { createSchemaFieldRule } from "antd-zod";
 import { z } from "zod/v4";
 
@@ -14,15 +13,15 @@ import { getErrMsg } from "@/utils/error";
 
 const TEMPLATE_KEY_STANDARD = "standard" as const;
 const TEMPLATE_KEY_CERTTEST = "monitor" as const;
-const TEMPLATE_KEY_BLANK = "blank" as const;
-type TemplateKeys = typeof TEMPLATE_KEY_BLANK | typeof TEMPLATE_KEY_CERTTEST | typeof TEMPLATE_KEY_STANDARD;
+const TEMPLATE_KEY_EMPTY = "empty" as const;
+type TemplateKeys = typeof TEMPLATE_KEY_EMPTY | typeof TEMPLATE_KEY_CERTTEST | typeof TEMPLATE_KEY_STANDARD;
 
 const WorkflowNew = () => {
   const navigate = useNavigate();
 
   const { t } = useTranslation();
 
-  const [notificationApi, NotificationContextHolder] = notification.useNotification();
+  const { notification } = App.useApp();
 
   const templateGridSpans = {
     xs: { flex: "100%" },
@@ -35,11 +34,11 @@ const WorkflowNew = () => {
 
   const formSchema = z.object({
     name: z
-      .string(t("workflow.new.modal.form.name.placeholder"))
+      .string()
       .min(1, t("workflow.new.modal.form.name.placeholder"))
       .max(64, t("common.errmsg.string_max", { max: 64 })),
     description: z
-      .string(t("workflow.new.modal.form.description.placeholder"))
+      .string()
       .max(256, t("common.errmsg.string_max", { max: 256 }))
       .nullish(),
   });
@@ -50,12 +49,16 @@ const WorkflowNew = () => {
     formProps,
     submit: submitForm,
   } = useAntdForm<z.infer<typeof formSchema>>({
+    initialValues: {
+      name: "",
+      description: "",
+    },
     onSubmit: async (values) => {
       try {
         let workflow: WorkflowModel;
 
         switch (templateSelectKey) {
-          case TEMPLATE_KEY_BLANK:
+          case TEMPLATE_KEY_EMPTY:
             workflow = initWorkflow();
             break;
 
@@ -76,7 +79,7 @@ const WorkflowNew = () => {
         workflow = await saveWorkflow(workflow);
         navigate(`/workflows/${workflow.id}`, { replace: true });
       } catch (err) {
-        notificationApi.error({ message: t("common.text.request_error"), description: getErrMsg(err) });
+        notification.error({ message: t("common.text.request_error"), description: getErrMsg(err) });
 
         throw err;
       }
@@ -110,18 +113,19 @@ const WorkflowNew = () => {
 
   return (
     <div>
-      {NotificationContextHolder}
-
-      <Card styles={{ body: { padding: "0.5rem", paddingBottom: 0 } }} style={{ borderRadius: 0 }}>
-        <PageHeader title={t("workflow.new.title")}>
-          <Typography.Paragraph type="secondary">{t("workflow.new.subtitle")}</Typography.Paragraph>
-        </PageHeader>
+      <Card styles={{ body: { padding: 0 } }}>
+        <div className="px-6 py-4">
+          <div className="mx-auto max-w-320">
+            <h1>{t("workflow.new.title")}</h1>
+            <p className="text-base text-gray-500">{t("workflow.new.subtitle")}</p>
+          </div>
+        </div>
       </Card>
 
       <div className="p-4">
-        <div className="mx-auto max-w-400 px-2">
+        <div className="mx-auto max-w-320 px-2">
           <Typography.Text type="secondary">
-            <div className="mb-8 mt-4 text-xl">{t("workflow.new.templates.title")}</div>
+            <div className="mt-4 mb-8 text-xl">{t("workflow.new.templates.title")}</div>
           </Typography.Text>
 
           <Row className="justify-stretch" gutter={[16, 16]}>
@@ -166,15 +170,15 @@ const WorkflowNew = () => {
                 className="size-full"
                 cover={<img className="min-h-[120px] object-contain" src="/imgs/workflow/tpl-blank.png" />}
                 hoverable
-                onClick={() => handleTemplateClick(TEMPLATE_KEY_BLANK)}
+                onClick={() => handleTemplateClick(TEMPLATE_KEY_EMPTY)}
               >
                 <div className="flex w-full items-center gap-4">
                   <Card.Meta
                     className="grow"
-                    title={t("workflow.new.templates.template.blank.title")}
-                    description={t("workflow.new.templates.template.blank.description")}
+                    title={t("workflow.new.templates.template.empty.title")}
+                    description={t("workflow.new.templates.template.empty.description")}
                   />
-                  <Spin spinning={templateSelectKey === TEMPLATE_KEY_BLANK} />
+                  <Spin spinning={templateSelectKey === TEMPLATE_KEY_EMPTY} />
                 </div>
               </Card>
             </Col>

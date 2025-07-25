8aa7f80042331c897b9309aafc05deec6190c663
Fu Diwei <fudiwei@sina.com>
2025-07-25 10:45:36 +0800

    fix: error occurred tring to get ssl certificates with a low version suite in monitoring

---
+++ pkg/utils/tls/config.go
@@ -0,0 +1,34 @@
+﻿package tls
+
+import (
+	"crypto/tls"
+)
+
+// 创建并返回一个兼容低版的 [tls.Config] 对象。
+//
+// 出参：
+//   - config: [tls.Config] 对象。
+func NewCompatibleConfig() *tls.Config {
+	var suiteIds []uint16
+	for _, suite := range tls.CipherSuites() {
+		suiteIds = append(suiteIds, suite.ID)
+	}
+	for _, suite := range tls.InsecureCipherSuites() {
+		suiteIds = append(suiteIds, suite.ID)
+	}
+
+	return &tls.Config{
+		MinVersion:   tls.VersionTLS10,
+		CipherSuites: suiteIds,
+	}
+}
+
+// 创建并返回一个不安全的 [tls.Config] 对象。
+//
+// 出参：
+//   - config: [tls.Config] 对象。
+func NewInsecureConfig() *tls.Config {
+	config := NewCompatibleConfig()
+	config.InsecureSkipVerify = true
+	return config
+}
